738a49ed4c11cc086e597172608b71f14457985a
Filippos Antoniadis <Director@Fils-book.local>
2018-03-01 18:11:58 +0000

    New commit

---
+++ js/main.js
@@ -0,0 +1,17 @@
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+
+var cardOne = cards[0];
+var cardTwo = cards[2];
+	
+cardsInPlay.push(cards[0]);
+cardsInPlay.push(cards[2]);
+
+console.log("User flipped " + cardsInPlay);
+
+if (cardsInPlay.length === 2)
+if (cardsInPlay[0] === cardsInPlay[1]) {
+alert("You found a match!");
+} else {
+alert("Sorry, try again.");
+}
